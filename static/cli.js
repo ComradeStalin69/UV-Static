f30972ccfc0918cd92367da5de968e0c99b740b0
ComradeStalin69 <derekjiang50@gmail.com>
2025-01-14 16:13:09 -0500

    Update cli.js

--- static/cli.js
+++ static/cli.js
@@ -14,8 +14,8 @@ document.addEventListener("DOMContentLoaded", () => {
             // Add an iframe for displaying web content
             const iframe = document.createElement("iframe");
             iframe.src = content;
-            iframe.style.width = "100%";
-            iframe.style.height = "900px";
+            iframe.style.width = "auto";
+            iframe.style.height = "auto";
             iframe.style.border = "none";
             container.appendChild(iframe);
         } else {
